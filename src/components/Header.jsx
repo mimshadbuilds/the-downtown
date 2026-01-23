2aad3f6ab0f33d36c5abe68bf9fc2a9fd59fcd50
Md.Mimshadur Rahman <rahman15-6119@diu.edu.bd>
2026-01-24 02:55:15 +0600

    main title replaced with logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
 import Navbar from './Navbar'
+import logo from '../assets/logo.png'
 export default function Header() {
     return (
         <header className="bg-white">
             <div className='container mx-auto px-4 py-4 text-center'>
-                <h1 className='text-4xl text-black font-bold tracking-wide'>THE DOWNTOWN</h1>
+            <div className="flex flex-col items-center gap-4">
+          <img 
+            src={logo} 
+            alt="" 
+            className="h-8 w-auto sm:h-10"
+          />
+        </div>
             </div>
             <Navbar />
         </header>
